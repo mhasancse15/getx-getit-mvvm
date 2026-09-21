769fe23b65c6b0cb3f37e171b2485d491ae75a0e
Mahmudul Hasan <m.mhasan@brainstation-23.com>
2026-09-21 18:33:22 +0600

    product details

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -2,4 +2,5 @@ abstract final class AppRoutes {
   static const String users = '/users';
   static const String userDetails = '/users/:id';
   static const String products = '/products';
+  static const String productDetails = '/products/:id';
 }
